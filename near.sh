ad44e4239bc6e18f86b7ea5586e79ddb2670c8b4
sparkmobi <rami_ali2010@hotmail.com>
2024-03-27 04:01:22 +0200

    Update near.sh

--- near.sh
+++ near.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RPC_URL="https://near.lava.build/lava-referer-8b51600b-b3cc-4893-89ec-5de41f0724db/"
+RPC_URL="https://near.lava.build/lava-referer-a4768e23-3e27-430a-aad8-fafd10b1615c/"
 
 OUTPUT_DIR="logs"
 
